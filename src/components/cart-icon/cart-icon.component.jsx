631903a859d2abe039a1e09118de10bd2e7f3139
divyasharma <divya.sharma1@publicissapient.com>
2022-09-09 12:34:23 +0530

    Cart icon hides on navigating to checkout  page

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,10 +1,12 @@
 import { useContext } from 'react';
+import { useLocation } from 'react-router-dom';
 import { ReactComponent as ShoppingBagIcon } from '../../assets/shopping-bag.svg';
 import { CartContext } from '../../contexts/cart.context';
 import './cart-icon.styles.scss';
 
 const CartIcon = () => {
     const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+    let { pathname } = useLocation();
 
     const toggleDropdown = () => {
         setIsCartOpen(!isCartOpen);
@@ -12,10 +14,15 @@ const CartIcon = () => {
     };
 
     return (
-        <div className='cart-icon-container' onClick={toggleDropdown}>
-            <ShoppingBagIcon className='shopping-icon' />
-            <span className='item-count'>0</span>
-        </div>
+        <>
+            {/* Since checkout page displays all cart items, the cart-icon should be hidden */}
+            {pathname !== '/checkout'
+                && (< div className='cart-icon-container' onClick={toggleDropdown} >
+                    <ShoppingBagIcon className='shopping-icon' />
+                    <span className='item-count'>0</span>
+                </div >)
+            }
+        </>
     );
 }
 
