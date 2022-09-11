e34e9c5d0f51d34230bb51a7d34ec6983e7a3389
divyasharma <divya.sharma1@publicissapient.com>
2022-09-11 18:16:37 +0530

    #1 code refactor

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -1,11 +1,16 @@
 import './category-item.styles.scss';
+import { useNavigate } from 'react-router-dom';
 
-const CategoryItem = ({ category: {imageUrl, type} }) => {
+const CategoryItem = ({ category: {imageUrl, type, route} }) => {
+    const navigate = useNavigate();
+
+    const onNavigateHandler = () => navigate(route);
+
     return (
-        <div className='category-container'>
+        <div className='category-item-container'>
             <div className='background-image' style={{ backgroundImage: `url(${imageUrl})` }}></div>
             <div className='category-body-container'>
-                <h2>{type}</h2>
+                <h2 onClick={onNavigateHandler}>{type}</h2>
                 <p>Shop Now</p>
             </div>
         </div>
