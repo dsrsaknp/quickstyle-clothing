539a2447d2b0040d8ea4a74e64fee08649636eb9
divyasharma <dsrsaknp@gmail.com>
2022-09-11 21:09:09 +0530

    warnings correction

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,4 +1,4 @@
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { useContext } from 'react';
 
 import { NavigationContainer, LogoContainer, NavLinks, NavLink } from './navigation.styles.jsx';
